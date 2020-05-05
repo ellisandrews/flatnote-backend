6f3436c0b9dd86ad7ebbe3363ac57b4b5d796d03
ellisandrews <ellisandrews1@gmail.com>
2020-05-05 13:11:11 -0500

    Add database-cleaner

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'database_cleaner/active_record'
+
+# Clean out the DB
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
