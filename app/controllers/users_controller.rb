a8eb9a0ab607e7b91fd22042f55c0849ee008f26
ellisandrews <ellisandrews1@gmail.com>
2020-05-06 13:22:13 -0500

    User serializtion now includes notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
 
+    # NOTE: Commented out actions are currently only necessary for development or admin purposes, so leaving out.
+
+    # def index
+    #     users = User.all.order(:id)
+    #     render json: users
+    # end
+
+    # def show
+    #     user = User.find_by(id: params[:id])
+    #     if user
+    #         render json: user
+    #     else
+    #         render json: { error: 'User not found' }, status: :not_found
+    #     end
+    # end
+
     def create
         user = User.find_or_create_by(user_params)
         render json: user
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :username, :notes
+
+  def notes
+    ActiveModelSerializers::SerializableResource.new(object.notes,  each_serializer: NoteSerializer)
+    # ActiveModel::SerializableResource.new(object.notes,  each_serializer: NoteSerializer)
+  end
+
 end
